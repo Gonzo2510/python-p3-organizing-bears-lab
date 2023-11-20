90be347e0be011c395c3eb5e832e6fb74b7a0a23
Jacob Gonzales <jagonz@live.com>
2023-11-19 19:05:57 -0600

    complete lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Mr.Chocolate', 4, 'M', 'White', 'Loyal', 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Rowdy', 2, 'M', 'Yellow', 'Extrovert', 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Tabitha', 4, 'F', 'Orange', 'Calm', 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Sergent Brown', 9, 'M', 'Brown', 'Social', 0);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Melissa', 6, 'F', 'Blue', 'Sensitive', 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Grinch', 4, 'M', 'Green', 'Bitter', 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ('Wendy', 2, 'F', 'Red', 'Fun', 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES (NULL, 1, 'M', 'Gray', NULL, 1);
